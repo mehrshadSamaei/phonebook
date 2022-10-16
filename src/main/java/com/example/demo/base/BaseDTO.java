1cc77977e96577cdee076cc4453e6977fc47afb9
mehrshad <mehrshadsamaei0074@gmail.com>
2022-10-16 10:54:16 +0330

    created base entity and dto

---
+++ src/main/java/com/example/demo/base/BaseDTO.java
@@ -0,0 +1,22 @@
+package com.example.demo.base;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@MappedSuperclass
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract class BaseDTO<ID extends Serializable> implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private ID id;
+    @Column(name = "is_active")
+    private Boolean isActive;
+}
