85b427ab442002ca463e343518a114a8dbf37479
mehrshad <mehrshadsamaei0074@gmail.com>
2022-10-17 20:52:52 +0330

    update mapper

---
+++ src/main/java/com/example/demo/mapper/request/UserRequestMapper.java
@@ -0,0 +1,11 @@
+package com.example.demo.mapper.request;
+
+import com.example.demo.base.BaseMapper;
+import com.example.demo.dto.phonebookdto.PhoneBookRequestDTO;
+import com.example.demo.dto.root.PhoneBookRootDTO;
+import com.example.demo.entity.PhoneBook;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface PhoneBookRequestMapper extends BaseMapper<PhoneBookRequestDTO, PhoneBook, Long> {
+}
