e7892236acb0292c82aa6b8a97ac5cc564f054fd
mehrshad <mehrshadsamaei0074@gmail.com>
2022-10-17 20:55:09 +0330

    userRequest mapper

--- src/main/java/com/example/demo/mapper/request/UserRequestMapper.java
+++ src/main/java/com/example/demo/mapper/request/UserRequestMapper.java
@@ -2,10 +2,11 @@ package com.example.demo.mapper.request;
 
 import com.example.demo.base.BaseMapper;
 import com.example.demo.dto.phonebookdto.PhoneBookRequestDTO;
-import com.example.demo.dto.root.PhoneBookRootDTO;
+import com.example.demo.dto.userdto.UserRequestDTO;
 import com.example.demo.entity.PhoneBook;
+import com.example.demo.entity.User;
 import org.mapstruct.Mapper;
 
 @Mapper
-public interface PhoneBookRequestMapper extends BaseMapper<PhoneBookRequestDTO, PhoneBook, Long> {
+public interface UserRequestMapper extends BaseMapper<UserRequestDTO, User, Long> {
 }
